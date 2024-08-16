afae63e7d101eee6fe713e7009e28fbc5e5bb077
Manzar <manzarinemanzar@gmail.com>
2024-08-16 13:53:54 +0545

    utils code

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,25 @@
 import dotenv from "dotenv"
 import connectDB from "./db/index.js"
+import express from "express"
 
 // Whenever we load this page, .env will be loaded first
 dotenv.config({
     path: './env'
 })
 
+const app = express()
+
+// connectDB is an asynchronous method. When it is executed, it technically returns a promise
 connectDB()
+.then(() => {
+    app.listen(process.env.PORT || 4000, () => {
+        console.log(`App is listening on port: ${process.env.PORT}`)
+    })
+    app.on("error", (error) => {
+        console.log("ERRR:: ", error)
+    })
+})
+.catch((error) => {
+    console.log("MONGODB CONNECTION FAILED:: ", error);
+    
+})
